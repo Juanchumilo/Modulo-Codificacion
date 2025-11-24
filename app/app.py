a592d3df84a1a91a7194278107105e3664e055c9
Juan Camilo <juankmilobernalz@gmail.com>
2025-11-24 14:06:09 -0500

    Todo perfecto, correccion de errores en editar y errores en uso de variables en editar y listar.html.
    Todo el sistema funciona correctamente y se conecta de forma correcta con la base de datos

--- app/app.py
+++ app/app.py
@@ -3,8 +3,13 @@ from db import get_connection
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/listar')
+def listar():
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM usuarios")
@@ -20,9 +25,47 @@ def crear():
 
         conn = get_connection()
         cursor = conn.cursor()
-        cursor.execute("INSERT INTO usuarios(nombre,correo) VALUES (%s,%s)", (nombre, correo))
+        cursor.execute("INSERT INTO usuarios(nombre, correo) VALUES (%s, %s)", (nombre, correo))
         conn.commit()
         conn.close()
+        return redirect('/listar')
 
-        return redirect('/')
     return render_template('crear.html')
+
+
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+
+        cursor.execute("UPDATE usuarios SET nombre=%s, correo=%s WHERE id=%s",(nombre, correo, id))
+        conn.commit()
+        conn.close()
+        return redirect('/listar')
+
+    cursor.execute("SELECT * FROM usuarios WHERE id=%s", (id,))
+    usuario = cursor.fetchone()
+    conn.close()
+
+    return render_template('editar.html', usuario=usuario)
+
+@app.route('/eliminar/<int:id>')
+def eliminar(id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM usuarios WHERE id=%s", (id,))
+    conn.commit()
+    conn.close()
+    return redirect('/listar')
+
+#Error 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+if __name__=='__main__':
+    app.run(debug=True)
--- app/db.py
+++ app/db.py
@@ -4,7 +4,8 @@ def get_connection():
     return pymysql.connect(
         host="localhost",
         user="root",
-        password="TU_PASSWORD",
+        password="2207Chumilo,",
         database="actividad_flask",
+        port=3306,
         cursorclass=pymysql.cursors.DictCursor
     )
